97d86ef8201240c409fd54764092250dbe44ab54
saraloncaric <167783165+saraloncaric@users.noreply.github.com>
2026-06-15 19:04:14 +0200

    initial commit

---
+++ src/main/java/com/example/concertlog/ConcertLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.concertlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertLogApplication.class, args);
+    }
+
+}
